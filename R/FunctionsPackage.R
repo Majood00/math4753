7734b446e847b5815367112e6992d35e2997af90
Majood Haddad <majood00@ou.edu>
2020-02-07 12:39:51 -0600

    modified Functions Package

--- R/FunctionsPackage.R
+++ R/FunctionsPackage.R
@@ -1,17 +1,16 @@
-#' My Standard Deviation Function
+#' My Standard Normal Distribution Functiom
 #'
-#' Creates standard deviation from vectors
+#' Finds standard normal distribution from vectors
 #'
-#' This is the first function in this package for the MATH4753 class. It takes the values of the vectors and plugs them into the equation. The answer shows the standard deviation or in other words, how much are the data far away from the mean value.
+#' This is the first function in this package for the MATH4753 class. It takes the values of the vectors and plugs them into the equation. The answer shows the standard normal distribution, or in other words, how much are the data far away from the mean value.
 #'
-#' @param x a vector in which its value is used to calculate the standard deviation
+#' @param x a vector in which its value is used to calculate the standard normal distribution
 #'
-#' @return a standard deviation vector on the form of a vector object
+#' @return a standard normal distribution vector on the form of a vector object
 #' @export
 #'
 #' @examples
-#' x=1:30;mysd(x)
-mysd = function(x){ # x is a vector
-  obj1=sqrt(sum((x-mean(x))^2)/(length(x)-1))
-  obj1
+#' x=1:30;mysnd(x)
+mysnd = function(x){
+  (x - mean(x)) / sd(x)
 }
