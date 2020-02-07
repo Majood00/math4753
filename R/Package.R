abddfc3fe000220f649230950da3cb9d624e2255
Majood Haddad <majood00@ou.edu>
2020-02-06 21:37:26 -0600

    R package

---
+++ R/Package.R
@@ -0,0 +1,10 @@
+#' Functions Package
+#'
+#' Math 4753 lab functions as produced by Majood Haddad.
+#'
+#' @docType package
+#'
+#' @author Majood Haddad \email{majood00@ou.edu}
+#'
+#' @name Package
+NULL
