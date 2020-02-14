ee42877c669a9865f16ed09e3f816a958297484d
Majood Haddad <majood00@ou.edu>
2020-02-13 23:35:18 -0600

    new function

---
+++ R/mynbinFunction.R
@@ -0,0 +1,18 @@
+#' My Negative Binomial Distribution Function
+#'
+#' Finds negative binomial distribution based on the parameters size, probabilities and the value of x. So, in other words, vecators are used to do the calculation
+#'
+#' This is the second function in this package for the MATH4753 class. It takes the values of the vectors and plugs them into the equation. The answer shows the negative binomial distribution, which is a discrete probability distribution in relation to the number of successes in a sequence of Bernoulli trials that are independent and identically distributed, and that is before a specified number of failures occurs.
+#'
+#' @param y a vector of non-negative integer quantiles
+#' @param r a vector which shows the size of the sample
+#' @param p a vector in which its value shows the probability
+#'
+#' @return a negative binomial distribution vector in the form of a vector object
+#' @export
+#'
+#' @examples
+#' y=5, r=12, p= 0.3;mynbino(y,r,p)
+mynbino=function(y,r,p){
+  choose(y-1,r-1)*p^r*(1-p)^(y-r)
+}
