93ec6e0cfe5977d080a0e48dfbd3f8fb1995a0bd
Majood Haddad <majood00@ou.edu>
2020-02-21 16:17:59 -0600

    commit please

--- R/myncurve.R
+++ R/myncurve.R
@@ -3,7 +3,7 @@
 #' Creates a curve of a normal distribution function and plots and shaded the area
 #'
 #' This is the tjird function in this package for the MATH4753 class. It takes the values of the vectors and plugs them into the equation. It creates a curve of a normal distribution, and shades the area created by the specific vectors and adds the value of the area to the curve.
-#' @param x a vector in which is value represents the upper limit for the x-axis
+#' @param a a vector in which is value represents the upper limit for the x-axis
 #' @param mu a vector representing the mean
 #' @param sigma a vector representing the standard deviation
 #'
@@ -12,12 +12,12 @@
 #'
 #' @examples
 #' x=5, mu=3, sigma=6;mycurve(x,mu,sigma)
-myncurve = function(x, mu, sigma){
-  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma)) #draws a normal curve
-  xcurve=seq(-∞,x,length=1000)  #finds the area between x=-∞ and x
+myncurve = function(mu, sigma, a){
+  curve(dnorm(a,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma)) #draws a normal curve
+  xcurve=seq(-∞,a,length=1000)  #finds the area between x=-∞ and x
   ycurve=dnorm(xcurve,mean,sd) #y values correspond to the x values
-  polygon(c(-∞,xcurve,x),c(0,ycurve,0),col="Orange") #fills in the polygon with the given vertices
-  prob=pnorm(x,mean,sd)-pnorm(-∞,mean,sd) #calclates or finds the area
+  polygon(c(-∞,xcurve,a),c(0,ycurve,0),col="Orange") #fills in the polygon with the given vertices
+  prob=pnorm(a,mean,sd)-pnorm(-∞,mean,sd) #calclates or finds the area
   prob=round(prob,4) #sets the area to 4 decimal places
-  text((-∞+x)/2,0.5*0.1,prob)  #plots the area
+  text((-∞+a)/2,0.5*0.1,prob)  #plots the area
 }
