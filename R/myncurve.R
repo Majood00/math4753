58e999bb515dc7f9811cd0eff3f9cd7e44adfaf7
Majood Haddad <majood00@ou.edu>
2020-02-21 16:28:47 -0600

    hmmm

--- R/myncurve.R
+++ R/myncurve.R
@@ -13,11 +13,11 @@
 #' @examples
 #' x=5, mu=3, sigma=6;mycurve(x,mu,sigma)
 myncurve = function(mu, sigma, x){
-  curve(dnorm(mean=mu,sd=sigma, x=a), xlim = c(mu-3*sigma, mu + 3*sigma)) #draws a normal curve
-  xcurve=seq(-1000,x,length=1000)  #finds the area between x=-∞ and x
-  ycurve=dnorm(xcurve,mean,sd) #y values correspond to the x values
-  polygon(c(-1000,xcurve,x),c(0,ycurve,0),col="Orange") #fills in the polygon with the given vertices
-  prob=pnorm(x,mean,sd)-pnorm(-1000,mean,sd) #calclates or finds the area
-  prob=round(prob,4) #sets the area to 4 decimal places
-  text((-1000+x)/2,0.5*0.1,prob)  #plots the area
+  curve(dnorm(mean=mu,sd=sigma, x=a), xlim = c( mu - 3*sigma, mu + 3*sigma))
+  xcurve=seq(-1000,x,length=1000)
+  ycurve=dnorm(xcurve,mean,sd)
+  polygon(c(-1000,xcurve,x),c(0,ycurve,0),col="Orange")
+  prob=pnorm(x,mean,sd)-pnorm(-1000,mean,sd)
+  prob=round(prob,4)
+  text((-1000+x)/2,0.5*0.1,prob)
 }
