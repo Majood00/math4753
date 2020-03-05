cc77b871be74a37c962d37018b1c9dbbca1a6807
Majood Haddad <majood00@ou.edu>
2020-03-05 15:50:27 -0600

    commit

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Fire Damage
+#'
+#' A data set collected concerning home damage from fire
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'  \item{Distance}{distance from fire station in miles}
+#'  \item{Damage}{X 1000 dollars in damage}
+#'
+#' @examples
+#' \dontrun{
+#'  firedam
+#' }
+#' @source \url{https:]]www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
