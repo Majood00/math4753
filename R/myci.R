c6be2c487f030714bc99e4af7f207d150d77a936
Majood Haddad <majood00@ou.edu>
2020-04-08 14:31:43 -0500

    ...

---
+++ R/myci.R
@@ -0,0 +1,16 @@
+#' 95% Confidence Interval Function for the Mean of a Sample
+#'
+#' Finds the 95% confidence interval for a sample with mean mu
+#'
+#' This is the 4th function in this package. This function produces a vector of an interval. This interval shows the confidence interval for the 95% confidence level of the mean of a sample which is being processed or which goes through this function.
+#'
+#' @param x vectore that stands for the sample
+#'
+#' @return vector with an interval for the 95% confidence level
+#' @export
+#'
+#' @examples
+#' set.seed(23);x = rnorm(30,mean=10,sd=12);myci(x)
+myci = function(x){
+  t.test(x,conf.level = 0.95)$conf.int
+}
