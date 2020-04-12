bffe29cd9e5a4d1954ebdf7891bcee2cc74a0ce2
Majood Haddad <majood00@ou.edu>
2020-04-12 11:12:22 -0500

    commit final final changes

--- R/mycifunction.R
+++ R/mycifunction.R
@@ -4,14 +4,14 @@
 #'
 #' This is the 4th function in this package. This function produces a vector of an interval. This interval shows the confidence interval for the 95% confidence level of the mean of a sample which is being processed or which goes through this function.
 #'
-#' @param x the vector of the sample we choose to find the confidence interval for
+#' @param x vector of the sample we choose to find the confidence interval for
 #'
-#' @return vector with an interval for the 95% confidence level
+#' @return vector with the 95% confidence interval for the sample input
 #' @export
 #'
 #' @examples
-#' set.seed(23)
-#' x = rnorm(30,mean=10,sd=12)
+#' set.seed(50)
+#' x=rnorm(30,mean=10,sd=5)
 #' myci(x)
 myci = function(x){
   t.test(x,mu=0,conf.level = 0.95)$conf.int
